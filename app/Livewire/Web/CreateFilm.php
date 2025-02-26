af2bf57b34508dd6458e54239d06ec0892f60bd6
luizhenrique <henriqueluiz1103@gmail.com>
2025-02-26 15:17:26 -0300

    Add createFilm

--- app/Livewire/Web/CreateFilm.php
+++ app/Livewire/Web/CreateFilm.php
@@ -2,10 +2,40 @@
 
 namespace App\Livewire\Web;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use App\Models\Film;
 
 class CreateFilm extends Component
 {
+    use WithFileUploads;
+
+    public $title;
+    public $summary;
+    public $cover;
+
+    protected $rules = [
+        'title' => 'required|min:3',
+        'summary' => 'nullable|min:5',
+        'cover' => 'image|max:1024'
+    ];
+
+    public function store()
+    {
+        $this->validate();
+
+        $coverPath = $this->cover->store('covers', 'public');
+
+        Film::create([
+            'title' => $this->title,
+            'summary' => $this->summary,
+            'cover' => $coverPath,
+        ]);
+
+        $this->reset(['title', 'summary','cover']);
+    }
+
     public function render()
     {
         return view('livewire.web.create-film');
--- resources/views/livewire/web/create-film.blade.php
+++ resources/views/livewire/web/create-film.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="max-w-screen-lg mx-auto px-4 py-8">
-        <form class="w-full px-4 py-8 bg-zinc-100 text-zinc-950 rounded-lg space-y-4">
+        <form wire:submit.prevent="store" class="w-full px-4 py-8 bg-zinc-100 text-zinc-950 rounded-lg space-y-4">
             <h1 class="text-2xl font-bold text-center font-film">
                 Cadastrar Filme
             </h1>
@@ -9,21 +9,21 @@
                 <label for="title" class="font-semibold">
                     Título
                 </label>
-                <input type="text" id="title" class="rounded-lg border border-zinc-950 p-2">
+                <input type="text" id="title" class="rounded-lg border border-zinc-950 p-2" wire:model="title">
             </div>
 
             <div class="flex flex-col gap-2">
                 <label for="summary" class="font-semibold">
                     Resumo
                 </label>
-                <textarea id="summary" rows="6" class="rounded-lg border border-zinc-950 p-2"></textarea>
+                <textarea id="summary" rows="6" class="rounded-lg border border-zinc-950 p-2" wire:model="summary"></textarea>
             </div>
 
             <div class="flex flex-col gap-2">
                 <label for="cover" class="font-semibold">
                     Capa
                 </label>
-                <input type="file" id="cover" class="rounded-lg border border-zinc-950 p-2">
+                <input type="file" wire:model="cover" id="cover" class="rounded-lg border border-zinc-950 p-2">
             </div>
 
             <button type="submit"
